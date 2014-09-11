5d9bb6e9d51c32325e819239f1a038409e710320
Kevin Mehall <km@kevinmehall.net>
2014-09-11 11:48:07 -0700

    Add module file example/test

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 
 extern crate yaml;
 use std::io::File;
+use std::os;
 mod specfile;
 
 fn main() {
-  let mut file = File::open(&Path::new("spi.yaml"));
+  let args = os::args();
+  let mut file = File::open(&Path::new(args[1].as_slice()));
 
   let doc = yaml::parse_io_utf8(&mut file).unwrap();
   let component: Result<specfile::Component, _> = specfile::FromYaml::from_yaml(&doc[0]);
